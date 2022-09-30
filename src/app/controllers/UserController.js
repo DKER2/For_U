1f715e7ec07e4bf0fce29c4f75c829e2634b3120
tquean15012003 <tquean15012003@gmail.com>
2022-10-01 02:48:32 +0800

    Update UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,21 +4,20 @@ const salt = "$2b$05$762uRTd20ZyeQnOM7S9oHO";
 
 const createUser = async (req, res) => {
     const d = new Date();
-    var { name, username, email, password } = req.body;
+    var { email, password } = req.body;
     let userID =  Math.floor(Math.random() * 100).toString() + d.getTime().toString();
 
     const hash = bcrypt.hashSync(password, salt);
     password = hash
 
     const newUser = {
-        userID, name, username, email, password
+        userID, name: "Harry", username: "tquean15012003", email, password
     }
 
     const user = new User(newUser);
     console.log(req.body);
     await user.save().then(() => {
-        console.log('Created Account')
-        res.send("Create Account")
+        res.send("Account Created!")
     })
 }
 
@@ -32,10 +31,10 @@ const signIn = async (req, res) => {
     const user = User.findOne({ 'email': email }).then(
         user => {
             if(user.password==hash){
-                res.send('Sign in Successful');
+                res.send('Sign in successfully!');
             }
             else{
-                res.send('Password is Wrong');
+                res.send('Password is incorrect!');
             }
         }
     ).catch((err) => {
